2333a5c6acabbbea493f05e434d2bfe79d3daa82
Shine Marson <ms27946@outlook.com>
2018-11-30 10:20:55 +0800

    fix:取消回调

--- ConcurrencyInCSharpCookbook/09Cancellation/CancellationRx.cs
+++ ConcurrencyInCSharpCookbook/09Cancellation/CancellationRx.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net.Http;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Threading;
@@ -11,6 +12,7 @@ namespace _09Cancellation {
     /// 可以订阅，取消订阅
     /// </summary>
     public class CancellationRx {
+        private const string m_className = nameof(CancellationRx);
         private IDisposable _subscrption;
         //方式1 通过订阅之后实现了 Idisposed 接口，所以取消订阅 只需要释放接口即可
         public void StartSubscribe() {
@@ -33,6 +35,38 @@ namespace _09Cancellation {
             int lastElement = await observable.TakeLast(10).ToTask(token);
             //或者
             int last = await observable.ToTask(token);
+        }
+
+        //注入取消请求
+        public async Task<HttpResponseMessage> GetWithTimeoutAsync(string url, CancellationToken cancellationToken) {
+            var client = new HttpClient();
+            using(var cts = CancellationTokenSource
+                .CreateLinkedTokenSource(cancellationToken)) {
+                cts.CancelAfter(TimeSpan.FromSeconds(2));
+                var combinedToken = cts.Token;
+                return await client.GetAsync(url, combinedToken);
+            }
+        }
+
+        //取消回调
+        public async Task CancellationCallback(CancellationToken token) {
+            token.Register(AfterCancelCallback);
+            await Task.Run(() => {
+                Console.WriteLine(m_className + " 执行中...");
+                Thread.Sleep(3000);
+            }, cancellationToken : token);
+            Console.WriteLine(m_className +
+                " 完成...");
+        }
+
+        public void Cancel(CancellationTokenSource cts) {
+            Console.WriteLine(m_className + " 取消中...");
+            Thread.Sleep(1000);
+            cts.Cancel();
+        }
+
+        private void AfterCancelCallback() {
+            Console.WriteLine(m_className + " 任务已取消 回调函数已调用 ");
         }
     }
 }
--- ConcurrencyInCSharpCookbook/09Cancellation/Program.cs
+++ ConcurrencyInCSharpCookbook/09Cancellation/Program.cs
@@ -7,18 +7,23 @@ namespace _09Cancellation {
     class Program {
         private static CancellationHelper canceller = new CancellationHelper();
         static void Main(string[] args) {
-            Task.Run(() => canceller.IssueTask());
-            Thread.Sleep(1000);
-            Cancel();
+            // Task.Run(() => canceller.IssueTask());
+            // Thread.Sleep(1000);
+            // Cancel();
 
-            CancellationByPoll cancellationByPoll = new CancellationByPoll();
-            Task.Run(() => cancellationByPoll.Execute());
-            Thread.Sleep(2000); //执行计算逻辑
-            cancellationByPoll.Cancel();
-            // Thread.Sleep(2000);
+            // CancellationByPoll cancellationByPoll = new CancellationByPoll();
+            // Task.Run(() => cancellationByPoll.Execute());
+            // Thread.Sleep(2000); //执行计算逻辑
+            // cancellationByPoll.Cancel();
+            // // Thread.Sleep(2000);
 
-            TimeoutAndCancellation timeoutAndCancellation = new TimeoutAndCancellation();
-            AsyncContext.Run(() => timeoutAndCancellation.IssueTimeoutAsync_v2());
+            // TimeoutAndCancellation timeoutAndCancellation = new TimeoutAndCancellation();
+            // AsyncContext.Run(() => timeoutAndCancellation.IssueTimeoutAsync_v2());
+
+            CancellationRx cancellationRx = new CancellationRx();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Task.Run(() => cancellationRx.CancellationCallback(cts.Token));
+            cancellationRx.Cancel(cts);
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
