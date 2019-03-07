382f9d398517df378874388d45fc9c29a6753be7
marsonshine <ms27946@163.com>
2019-03-08 01:01:23 +0800

    feat:handle exception in java concurrency situations

---
+++ ThinkingInJava/src/concurrency/ExceptionUnCaughtException.java
@@ -0,0 +1,42 @@
+package concurrency;
+
+import java.util.concurrent.*;
+
+class ExceptionThread2 implements Runnable {
+    @Override
+    public void run() {
+        Thread t = Thread.currentThread();
+        System.out.println("run() by " + t);
+        System.out.println("eh = " + t.getUncaughtExceptionHandler());
+        throw new RuntimeException();
+    }
+}
+
+class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("caught " + e);
+    }
+}
+
+class HandlerThreadFactory implements ThreadFactory {
+    @Override
+    public Thread newThread(Runnable r) {
+        System.out.println(this + "creating new Thread");
+        Thread t = new Thread(r);
+        System.out.println("created " + t);
+        t.setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+        return t;
+    }
+}
+
+/**
+ * ExceptionUnCaughtException
+ */
+public class ExceptionUnCaughtException {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool(new HandlerThreadFactory());
+        exec.execute(new ExceptionThread2());
+
+    }
+}
