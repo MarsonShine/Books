f3cdd40e3d01154f89639d27c7c4ecbceb302973
marsonshine <ms27946@outlook.com>
2019-07-16 14:04:15 +0800

    fix: http://www.typescriptlang.org/docs/handbook/advanced-types.html

--- ts/src/advance/generic.ts
+++ ts/src/advance/generic.ts
@@ -51,3 +51,65 @@ let x = { a: 1, b: 2, c: 3, d: 4 };
 
 getProperty(x, "a"); // okay
 // getProperty(x, "m"); // error: Argument of type 'm' isn't assignable to 'a' | 'b' | 'c' | 'd';
+
+//
+function loggingIdentity<T>(arg: Array<T>): Array<T> {
+    console.log(arg.length);
+    return arg;
+}
+//泛型类型
+function identity<T>(arg: T): T {
+    return arg;
+}
+let myIdentity: <T>(arg: T) => T = identity;
+//对象字面量
+let myIdentity2: { <T>(arg: T): T } = identity;
+
+interface GenericIdentityFunc<T> {
+    (arg: T): T;
+}
+
+let myIdentity3: GenericIdentityFunc<number> = identity;
+
+class GenericNumber<T>{
+    zeroValue: T;
+    add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+
+myGenericNumber.zeroValue = 26;
+myGenericNumber.add = (x, y) => x + y;
+//在泛型里使用类类型
+function create<T>(c: { new(): T; }): T {
+    return new c();
+}
+
+
+//使用原型属性推断并约束构造函数与类实例的关系。
+class BeeKeeper {
+    hasMask: boolean;
+}
+
+class ZooKeeper {
+    nametag: string;
+}
+
+class Animal_D {
+    numLegs: number;
+}
+
+class Bee extends Animal_D {
+    keeper: BeeKeeper;
+}
+
+class Lion extends Animal_D {
+    keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal_D>(c: new () => A): A {
+    return new c();
+}
+
+createInstance(Lion).keeper.nametag;
+createInstance(Bee).keeper.hasMask;
