494924aca8b894216781012433705706bd6bc5c4
marsonshine <ms27946@outlook.com>
2019-08-07 10:10:59 +0800

    feat:async stream,pattern matching

---
+++ CSharpGuide/LanguageVersions/8.0/AsyncStream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGuide.LanguageVersions._8._0
+{
+    public class AsyncStream
+    {
+        //申明异步流
+        internal async IAsyncEnumerable<int> GenerateSequence()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                //每三个元素等待2秒
+                if (i % 3 == 0)
+                    await Task.Delay(TimeSpan.FromSeconds(2));
+                yield return i;
+            }
+        }
+
+        internal async Task<int> ConsumeStream()
+        {
+            await foreach (var number in GenerateSequence())
+            {
+                Console.WriteLine($"The time is {DateTime.Now:hh:mm:ss}. Retrieved {number}");
+            }
+            return 0;
+        }
+    }
+}
---
+++ CSharpGuide/LanguageVersions/8.0/SwitchExpression.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGuide.LanguageVersions._8._0
+{
+    class SwitchExpression
+    {
+        public static RGBColor FromRainbow(Rainbow colorBand) => colorBand switch
+        {
+            Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
+            Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
+            _ => throw new ArgumentException(message: "invalid enum value,", paramName: nameof(colorBand)),
+        };
+
+        public static string GetGradeKindsof(decimal points)
+        {
+            switch (points)
+            {
+                case decimal p when (p < 60):
+                    return "不及格";
+                case decimal p when p >= 60 && p < 80:
+                    return "一般";
+                case decimal p when p >= 80 && p <= 100:
+                    return "优秀";
+                default:
+                    return "";
+            }
+
+        }
+
+        public static string GetGradeKindsofUsingCharpEight(decimal points) => points switch
+        {
+            decimal p when p < 60 => "不及格",
+            decimal p when p >= 60 && p < 80 => "一般",
+            decimal p when p >= 80 && p <= 100 => "优秀",
+            _ => throw new ArgumentException(nameof(points)),
+        };
+    }
+
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+
+    public class RGBColor
+    {
+        public RGBColor(int r, int g, int b)
+        {
+
+        }
+    }
+}
