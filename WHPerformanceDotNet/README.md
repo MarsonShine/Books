9fe10c56e6feba7d366ca8663a029e8b22d32d68
marsonshine <ms27946@outlook.com>
2020-05-05 00:31:38 +0800

    feat: 《编写高性能 .NET 代码》异步编程篇

--- WHPerformanceDotNet/README.md
+++ WHPerformanceDotNet/README.md
@@ -1,3 +1,4 @@
 # 《编写高性能的 .NET 代码》学习笔记
 
 1. [垃圾回收](docs/GC.md)
+2. [异步编程](docs/AsyncProgram.md)
---
+++ WHPerformanceDotNet/src/AsyncProgram/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgram {
+    class Program {
+        static Stopwatch watch = new Stopwatch();
+        static int peddingTasks;
+        static void Main(string[] args) {
+            const int MaxValue = 100000000;
+
+            watch.Restart();
+            int numTasks = Environment.ProcessorCount;
+            peddingTasks = numTasks;
+            int preThreadCount = MaxValue / numTasks;
+            int perThreadLeftover = MaxValue % numTasks;
+
+            var tasks = new Task<long>[numTasks];
+
+            for (int i = 0; i < numTasks; i++) {
+                int start = i * preThreadCount;
+                int end = (i + 1) * preThreadCount;
+                if (i == numTasks - 1) {
+                    end += perThreadLeftover;
+                }
+                tasks[i] = Task<long>.Run(() => {
+                    long threadSum = 0;
+                    for (int j = 0; j <= end; j++) {
+                        threadSum += (long) Math.Sqrt(j);
+                    }
+                    return threadSum;
+                });
+                tasks[i].ContinueWith(OnTaskEnd);
+            }
+        }
+
+        private static void OnTaskEnd(Task<long> task) {
+            Console.WriteLine($"Thread sum: {task.Result}");
+            if (Interlocked.Decrement(ref peddingTasks) == 0) {
+                watch.Stop();
+                Console.WriteLine($"Tasks: {watch.Elapsed}");
+            }
+        }
+    }
+}
