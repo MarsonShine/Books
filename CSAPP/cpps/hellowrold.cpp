e66e52a40cad98b6ade103b98c133e0d2004045b
marsonshine <ms27946@outlook.com>
2020-05-26 09:27:39 +0800

    fix: 程序结构与执行 进制数转换

--- CSAPP/cpps/hellowrold.cpp
+++ CSAPP/cpps/hellowrold.cpp
@@ -14,3 +14,4 @@ int main()
     }
     cout << endl;
 }
+
---
+++ CSAPP/cpps/showbytes.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+
+void show_bytes(byte_pointer start,size_t len) {
+    size_t i;
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%.2x",start[i]);
+    }
+    printf("\n");
+    
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
