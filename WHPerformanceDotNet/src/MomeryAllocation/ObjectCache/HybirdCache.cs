8729669f03b3a76d836245d40559c92262961a32
marsonshine <ms27946@outlook.com>
2020-07-27 18:05:29 +0800

    feat: WeakReference 对象缓存使用

---
+++ WHPerformanceDotNet/src/MomeryAllocation/ObjectCache/HybirdCache.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+/// <summary>
+/// doc form https://docs.microsoft.com/en-us/dotnet/standard/garbage-collection/weak-references
+/// 一个对象被应用程序引用时，垃圾回收器无法收集这个对象。
+/// weakreference 允许垃圾回收器对其应用程序引用的对象回收。
+/// 弱引用在对象占用大量内存时特别有用，垃圾回收器回收了这些对象，但是能很容易的重新生成这些对象。
+/// </summary>
+namespace MomeryAllocation.ObjectCache
+{
+    /// <summary>
+    /// <see cref="WeakReference{T}"/> 实现对象缓存，避免内存分配的开销的例子
+    ///  内部实现了两种缓存
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class HybirdCache<TKey, TValue>
+        where TValue : class
+    {
+        class ValueContainer<T>
+        {
+            public T value;
+            public long additionTime;
+            public long demoteTime;
+        }
+
+        private readonly TimeSpan maxAgeBeforeDemotion;
+        // Values 生存在这里，直到触发它们的 maxAge 值
+        private readonly ConcurrentDictionary<TKey, ValueContainer<TValue>> strongReferences =
+            new ConcurrentDictionary<TKey, ValueContainer<TValue>>();
+        // Values 在触发 maxAge 值移除到这里
+        private readonly ConcurrentDictionary<TKey, WeakReference<ValueContainer<TValue>>> weakReferences =
+            new ConcurrentDictionary<TKey, WeakReference<ValueContainer<TValue>>>();
+
+        public int Count => this.strongReferences.Count;
+        public int WeakCount => this.weakReferences.Count;
+
+        public HybirdCache(TimeSpan maxAgeBeforeDemotion)
+        {
+            this.maxAgeBeforeDemotion = maxAgeBeforeDemotion;
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            RemoveFromWeak(key);
+            var container = new ValueContainer<TValue>
+            {
+                value = value,
+                additionTime = Stopwatch.GetTimestamp(),
+                demoteTime = 0
+            };
+            this.strongReferences.AddOrUpdate(key, container, (key, value) => container);
+        }
+
+        private void RemoveFromWeak(TKey key)
+        {
+            weakReferences.TryRemove(key, out _);
+        }
+
+        public bool TryGetValue(TKey key,out TValue value)
+        {
+            value = null;
+            if(this.strongReferences.TryGetValue(key,out var container))
+            {
+                AttemptADemotion(key, container);
+                value = container.value;
+                return true;
+            }
+            if(this.weakReferences.TryGetValue(key,out var weakContainer))
+            {
+                if(weakContainer.TryGetTarget(out var valueContainer))
+                {
+                    value = valueContainer.value;
+                    return true;
+                }
+                else
+                {
+                    RemoveFromWeak(key);
+                }
+            }
+            return false;
+        }
+
+        private void AttemptADemotion(TKey key, ValueContainer<TValue> container)
+        {
+            long now = Stopwatch.GetTimestamp();
+            var age = CalculateTimeSpan(container.additionTime, now);
+            if (age > this.maxAgeBeforeDemotion) {
+                Demote(key, container);
+            }
+        }
+
+        private TimeSpan CalculateTimeSpan(long offsetA, long offsetB)
+        {
+            long diff = offsetB - offsetA;
+            double seconds = (double)diff / Stopwatch.Frequency;
+            return TimeSpan.FromSeconds(seconds);
+        }
+
+        private void Demote(TKey key, ValueContainer<TValue> container)
+        {
+            this.strongReferences.TryRemove(key, out ValueContainer<TValue> oldContainer);
+            container.demoteTime = Stopwatch.GetTimestamp();
+            var weakRef = new WeakReference<ValueContainer<TValue>>(container);
+            this.weakReferences.AddOrUpdate(key, weakRef, (k, v) => weakRef);
+        }
+    }
+}
---
+++ WHPerformanceDotNet/src/MomeryAllocation/ObjectCache/SecondaryIndex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace MomeryAllocation.ObjectCache
+{
+    public class SecondaryIndex
+    {
+        class Person
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime Birthday { get; set; }
+        }
+
+        class PersonDatabase
+        {
+            private Dictionary<string, Person> index = new Dictionary<string, Person>();
+            private Dictionary<DateTime, List<WeakReference<Person>>> birthdayIndex = new Dictionary<DateTime, List<WeakReference<Person>>>();
+
+            public bool NeedsIndexRebuild { get; private set; }
+            public void AddPerson(Person person)
+            {
+                this.index[person.Id] = person;
+                if (!this.birthdayIndex.TryGetValue(person.Birthday, out List<WeakReference<Person>> birthdayList))
+                {
+                    birthdayIndex[person.Birthday] = birthdayList = new List<WeakReference<Person>>();
+                }
+
+                birthdayList.Add(new WeakReference<Person>(person));
+            }
+
+            public void RemovePerson(string id) => index.Remove(id);
+
+            public bool TryGetById(string id, out Person person) => this.index.TryGetValue(id, out person);
+
+            public bool TryGetByBirthday(DateTime birthday,out List<Person> people)
+            {
+                people = null;
+                if(this.birthdayIndex.TryGetValue(birthday,out List<WeakReference<Person>> weakRef))
+                {
+                    var list = new List<Person>(weakRef.Count);
+                    foreach (var wp in weakRef)
+                    {
+                        Person person;
+                        if(wp.TryGetTarget(out person))
+                        {
+                            list.Add(person);
+                        }
+                        else
+                        {
+                            // 否则就获取了一个 null 引用
+                            this.NeedsIndexRebuild = true;
+                        }
+                    }
+                    if(list.Count > 0)
+                    {
+                        people = list;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
