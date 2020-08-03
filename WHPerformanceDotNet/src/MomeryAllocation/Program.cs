7bce0d71f9183d86d3ec47dc0e9cf6047409dacc
marsonshine <ms27946@outlook.com>
2020-08-03 11:50:27 +0800

    fix: 泛型底层原理

--- WHPerformanceDotNet/src/MomeryAllocation/Program.cs
+++ WHPerformanceDotNet/src/MomeryAllocation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MomeryAllocation.MemoryAllocationOfSpan;
+using MomeryAllocation.WinDbg;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,59 +7,64 @@ using System.Collections.Generic;
 namespace MomeryAllocation {
     class Program {
         static void Main(string[] args) {
-            const int size = 1000 * 1000;
-            // var empty = new Empty();
-            var before = GC.GetTotalMemory(true);
-            var empty = new Empty();
-            var after = GC.GetTotalMemory(true);
+            //const int size = 1000 * 1000;
+            //// var empty = new Empty();
+            //var before = GC.GetTotalMemory(true);
+            //var empty = new Empty();
+            //var after = GC.GetTotalMemory(true);
 
-            var diff = after - before;
+            //var diff = after - before;
 
-            Console.WriteLine("空对象内存大小：" + diff);
+            //Console.WriteLine("空对象内存大小：" + diff);
 
-            GC.KeepAlive(empty);
+            //GC.KeepAlive(empty);
 
-            // 数组对象
-            var array = new Empty[size];
-            before = GC.GetTotalMemory(true);
-            for (int i = 0; i < size; i++) {
-                array[i] = new Empty();
-            }
-            after = GC.GetTotalMemory(true);
+            //// 数组对象
+            //var array = new Empty[size];
+            //before = GC.GetTotalMemory(true);
+            //for (int i = 0; i < size; i++) {
+            //    array[i] = new Empty();
+            //}
+            //after = GC.GetTotalMemory(true);
 
-            diff = after - before;
-            Console.WriteLine("数组空对象内存带下：" + diff / size);
-            GC.KeepAlive(array);
+            //diff = after - before;
+            //Console.WriteLine("数组空对象内存带下：" + diff / size);
+            //GC.KeepAlive(array);
 
-            var vt = new ValueTypeBox();
-            vt.Name = "summer zhu";
-            InvokeWithStructClass(vt);
-            Console.WriteLine(vt.Name);
-            // 值类型当参数传递
-            void InvokeWithStructClass(ValueTypeBox box) {
-                box.Name = "marson shine";
-                Console.WriteLine(box.Name);
-            }
+            //var vt = new ValueTypeBox();
+            //vt.Name = "summer zhu";
+            //InvokeWithStructClass(vt);
+            //Console.WriteLine(vt.Name);
+            //// 值类型当参数传递
+            //void InvokeWithStructClass(ValueTypeBox box) {
+            //    box.Name = "marson shine";
+            //    Console.WriteLine(box.Name);
+            //}
 
-            // for vs foreach
-            int[] arr = new int[100];
-            for (int i = 0; i < arr.Length; i++) {
-                arr[i] = i;
-            }
+            //// for vs foreach
+            //int[] arr = new int[100];
+            //for (int i = 0; i < arr.Length; i++) {
+            //    arr[i] = i;
+            //}
 
-            int sum = 0;
-            foreach (var val in arr) {
-                sum += val;
-            }
+            //int sum = 0;
+            //foreach (var val in arr) {
+            //    sum += val;
+            //}
 
-            sum = 0;
-            IEnumerable<int> arrEnum = arr;
-            foreach (var val in arrEnum) {
-                sum += val;
-            }
+            //sum = 0;
+            //IEnumerable<int> arrEnum = arr;
+            //foreach (var val in arrEnum) {
+            //    sum += val;
+            //}
 
 
-            AvoidMemoryAlloctation.Start();
+            //AvoidMemoryAlloctation.Start();
+
+            // dump 分析
+            var ec = new ExampleClass();
+
+            var ecg = new ExampleGenericClass<object>();
         }
     }
 }
---
+++ WHPerformanceDotNet/src/MomeryAllocation/WinDbg/ExampleClass.cs
@@ -0,0 +1,16 @@
+﻿namespace MomeryAllocation.WinDbg
+{
+    public class ExampleClass
+    {
+        private int _myField;
+
+        public int MyMethod() => _myField;
+    }
+
+    public class ExampleGenericClass<T>
+    {
+        private T _myField;
+
+        public T MyMethod() => _myField;
+    }
+}
