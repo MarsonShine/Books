dee54f20af41d0bcea2d8f503818d8d2743a5058
marsonshine <ms27946@outlook.com>
2020-08-10 18:09:08 +0800

    fix: 线程安全的随机数生成

--- WHPerformanceDotNet/src/LockFreeWithInterlocked/Program.cs
+++ WHPerformanceDotNet/src/LockFreeWithInterlocked/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace LockFreeWithInterlocked {
-    class Program {
-        static void Main(string[] args) {
+namespace LockFreeWithInterlocked
+{
+    class Program
+    {
+        // ThreadLocal 主要作用就是让各个线程维持自己的变量
+        private readonly static ThreadLocal<Random> threadLocalRand =
+            new ThreadLocal<Random>(() =>
+            {
+                return new Random();
+            });
+
+        static void Main(string[] args)
+        {
             LockFreeStack<string> lockFree = new LockFreeWithInterlocked.LockFreeStack<string>();
             lockFree.Push("marson shine");
             lockFree.Push("summer zhu");
 
             string name = lockFree.Pop();
             Console.WriteLine("Hello World!");
+
+            int[] results = new int[100];
+            Parallel.For(0, 5000, i =>
+            {
+                var randomNumber = threadLocalRand.Value.Next(100);
+                Interlocked.Increment(ref results[randomNumber]);
+            });
+
+            Console.WriteLine(string.Join(' ', results.Select((p, i) => $"results[{i}] = {p}")));
+            Console.ReadLine();
+
         }
     }
 }
