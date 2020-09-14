ad44eb49e68805f1636f9ce037451199658cfcd7
marsonshine <ms27946@outlook.com>
2020-09-14 19:34:15 +0800

    fix: 循环展开，减少循环次数

--- CSAPP/cpps/csapp/05opcode/optvec.c
+++ CSAPP/cpps/csapp/05opcode/optvec.c
@@ -100,6 +100,25 @@ void combine4(vec_ptr v, data_t* dest) {
 	*dest = acc;
 }
 
+void combine5(vec_ptr v, data_t* dest) {
+	long i = 0;
+	long length = vec_length(v);
+	long limit = length - 1;
+	data_t* data = get_vec_start(v);
+	data_t acc = IDENT;
+
+	/* 一次计算两个元素 */
+	for (i = 0; i < limit; i += 2)
+	{
+		acc = (acc OP data[i]) OP data[i + 1];
+	}
+	/* 完成剩下的元素 */
+	for (; i < length; i++)
+	{
+		acc = acc OP data[i];
+	}
+}
+
 void lower1(char* s) {
 	long i;
 	for (i = 0; i < strlen(s); i++)
