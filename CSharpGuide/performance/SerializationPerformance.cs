411f4a06d7fa451d00226c73318f271535b68d75
marsonshine <ms27946@outlook.com>
2020-11-11 10:29:31 +0800

    fix: system.text.json 序列化

---
+++ CSharpGuide/performance/SerializationPerformance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CSharpGuide.performance
+{
+    public class SerializationPerformance
+    {
+        // TODO
+    }
+
+    public class RequestBody
+    {
+        [JsonConstructor]
+        public RequestBody(string name,int age,Class @class)
+        {
+            Name = name;
+            Age = age;
+            Class = @class;
+        }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Description { get; set; }
+        public Class? Class { get; set; }
+    }
+
+    public class Class
+    {
+        public int ClassId { get; set; }
+        [AllowNull]
+        public string ClassName { get; set; }
+        public int SchoolId { get; set; }
+        [AllowNull]
+        public string SchoolName { get; set; }
+    }
+}
