e21cc02e3c9bea5db6b22ffa43a74ba7024cfca1
marsonshine <ms27946@outlook.com>
2021-04-19 10:47:27 +0800

    feat: c#7 解构与模式匹配

---
+++ CSharpInDepth/Chapter12_Deconstruction_PatnerMatching/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter12_Deconstruction_PatnerMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
