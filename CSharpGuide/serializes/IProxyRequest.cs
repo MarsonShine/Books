55295d7505911ba6462e1c196c404a0945b6435e
marsonshine <ms27946@outlook.com>
2021-09-01 16:52:31 +0800

    fix: json utf8writer，jsondocument 使用方式

---
+++ CSharpGuide/serializes/IProxyRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace CSharpGuide.serializes
+{
+    public interface IProxyRequest
+    {
+        void WriteJson(Utf8JsonWriter writer);
+    }
+
+    public class IndexRequest<TDocument> : IProxyRequest
+    {
+        public TDocument? Document { get; set; }
+
+        public void WriteJson(Utf8JsonWriter writer)
+        {
+            if (Document is null) return;
+
+
+        }
+    }
+}
---
+++ CSharpGuide/serializes/JsonDocumentOps.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CSharpGuide.serializes
+{
+    public class JsonDocumentOps
+    {
+        const string jsonBody = @"
+{
+  'Class Name': 'Science',
+  'Teacher\u0027s Name': 'Jane',
+  'Semester': '2019-01-01',
+  'Students': [
+    {
+      'Name': 'John',
+      'Grade': 94.3
+    },
+    {
+      'Name': 'James',
+      'Grade': 81.0
+    },
+    {
+    'Name': 'Julia',
+      'Grade': 91.9
+    },
+    {
+    'Name': 'Jessica',
+      'Grade': 72.4
+    },
+    {
+    'Name': 'Johnathan'
+    }
+  ],
+  'Final': true
+}
+";
+
+        void ReadJson()
+        {
+            double sum = 0;
+            int count = 0;
+
+            using var document = JsonDocument.Parse(jsonBody);
+            JsonElement root = document.RootElement;
+            JsonElement studentsElement = root.GetProperty("Students");
+
+            count = studentsElement.GetArrayLength();   // 可以快速获取数量，不用循环+1
+
+            foreach (JsonElement student in studentsElement.EnumerateArray())
+            {
+                if (student.TryGetProperty("Grade", out JsonElement gradeElement))
+                {
+                    sum += gradeElement.GetDouble();
+                }
+                else
+                {
+                    sum += 70;
+                }
+                //count++;
+            }
+            double average = sum / count;
+            Console.WriteLine($"Average grade : {average}");
+        }
+
+        void WriteJson()
+        {
+            //string jsonString = File.ReadAllText(inputFileName);
+            string jsonString = jsonBody;
+            var writerOptions = new JsonWriterOptions
+            {
+                Indented = true
+            };
+
+            var documentOptions = new JsonDocumentOptions
+            {
+                CommentHandling = JsonCommentHandling.Skip
+            };
+
+            using MemoryStream ms = new();
+            using var writer = new Utf8JsonWriter(ms, options: writerOptions);
+            using JsonDocument document = JsonDocument.Parse(jsonString, documentOptions);
+
+            JsonElement root = document.RootElement;
+
+            if (root.ValueKind == JsonValueKind.Object)
+            {
+                writer.WriteStartObject();
+            }
+            else
+            {
+                return;
+            }
+            foreach (JsonProperty property in root.EnumerateObject())
+            {
+                property.WriteTo(writer);
+            }
+
+            writer.WriteEndObject();
+
+            writer.Flush();
+        }
+    }
+}
