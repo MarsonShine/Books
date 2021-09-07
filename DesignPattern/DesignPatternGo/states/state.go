af80b21b992877820244452a7798fc58ee4b58e5
marsonshine <ms27946@outlook.com>
2021-09-07 17:02:34 +0800

    feat: 状态模式，利用各自上下文来消除各种if判断

---
+++ DesignPattern/DesignPatternGo/states/state.go
@@ -0,0 +1,56 @@
+package states
+
+import (
+	"fmt"
+	"log"
+)
+
+// from https://github.com/RefactoringGuru/design-patterns-go/
+type state interface {
+	requestItem() error
+	addItem(int) error
+	insertMoney(money int) error
+	dispenseItem() error
+}
+
+func Start() {
+	vendingMachine := newVendingMachine(1, 10)
+	err := vendingMachine.requestItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.insertMoney(10)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.dispenseItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println()
+
+	err = vendingMachine.addItem(2)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println()
+
+	err = vendingMachine.requestItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.insertMoney(10)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = vendingMachine.dispenseItem()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
