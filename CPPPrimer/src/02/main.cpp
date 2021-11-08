b99a90fb59c1a71fe8429787d16edac4ca37f17e
marsonshine <ms27946@outlook.com>
2021-11-08 18:48:13 +0800

    feat: 基本数据类型，long，double，符号无符号数

---
+++ CPPPrimer/src/02/main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    float f = 123456789.123456789;
+    printf("%f\n",f);
+
+    f = 123456789.654321;
+    printf("%f\n",f);
+
+    double l = 12345678998765.65656;
+    printf("%lf\n",l);
+    return 0;
+}
