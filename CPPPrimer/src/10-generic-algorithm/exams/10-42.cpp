34174ad761fc2893557b2ae5424fd7a116741798
marsonshine <ms27946@outlook.com>
2022-02-26 00:01:50 +0800

    fix: 链表数据结构，list,forward_list

---
+++ CPPPrimer/src/10-generic-algorithm/exams/10-42.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <string>
+
+// list 去重
+int main()
+{
+    std::list<std::string> ls{"a", "aa", "aa", "aa", "aass", "asssssaa", "aa"};
+    ls.sort();
+    ls.unique();
+
+    for (auto &e : ls)
+    {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+    
+
+    // list 自定义排序
+    ls.sort([](const std::string& a, const std::string& b) { return a.size() <= b.size(); });
+}
