9ede590f8b912f3bb40765bb562ae49da2af43fb
marsonshine <ms27946@outlook.com>
2022-06-29 22:59:42 +0800

    fix: function composite

---
+++ FunctionalCSharp/src/Demo/Examples/5/FunctionComposite.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Examples._5
+{
+    // 函数组合：多个函数组合成一个新函数
+    // h=f*g -> h(x)=(f*g)(x) -> f(g(x))
+    public class FunctionComposite
+    {
+        static string AbbreviateName(Person p) => Abbreviate(p.FirstName) + Abbreviate(p.LastName);
+        static string AppendDomain(string localPart) => $"{localPart}@manning.com";
+        static string Abbreviate(string? s) => s!.Substring(0, 2).ToLower();
+
+        public static void Main() {
+            Func<Person, string> emailFor = p => AppendDomain(AbbreviateName(p));
+            var joe = new Person { LastName = "Marson", FirstName = "Shine"};
+            var email = emailFor(joe);
+
+            // v2
+            var marsonShine = new Person{LastName = "Marson", FirstName = "Shine"};
+            var email2 = joe.AbbreviateName().AppendDomain();
+
+        }
+    }
+    // 以上方法可以改为拓展函数 -> 链式表达式能提高可读性
+    public static class FunctionCompositeExtensions {
+        public static string AbbreviateName(this Person p) => Abbreviate(p.FirstName) + Abbreviate(p.LastName);
+        public static string AppendDomain(this string localPart) => $"{localPart}@manning.com";
+        static string Abbreviate(string? s) => s!.Substring(0, 2).ToLower();
+    }
+}
