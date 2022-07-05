8a836860d6659f1a3988aa4a7bec1b77238acdb8
marsonshine <ms27946@outlook.com>
2022-07-05 18:19:09 +0800

    fix: exceptional，try

---
+++ FunctionalCSharp/src/MarsonShine.Functional/Exceptional.cs
@@ -0,0 +1,38 @@
+﻿namespace MarsonShine.Functional
+{
+    using Unit = ValueTuple;
+    public static partial class F
+    {
+        public static Exceptional<T> Exceptional<T>(T value) => new Exceptional<T>(value);
+    }
+
+    public struct Exceptional<T>
+    {
+        internal Exception? Ex { get; }
+        internal T Value { get; }
+
+        public bool Success => Ex == null;
+        public bool Exception => Ex != null;
+
+        internal Exceptional(Exception ex)
+        {
+            Ex = ex ?? throw new ArgumentNullException(nameof(ex));
+            Value = default;
+        }
+
+        internal Exceptional(T right)
+        {
+            Value = right;
+            Ex = null;
+        }
+
+        public static implicit operator Exceptional<T>(T right) => new(right);
+        public static implicit operator Exceptional<T>(Exception left) => new(left);
+
+        public TR Match<TR>(Func<Exception, TR> Exception, Func<T, TR> Success) => this.Exception ? Exception(Ex) : Success(Value);
+        public Unit Match(Action<Exception> Exception, Action<T> Success) => Match(Exception.ToFunc(), Success.ToFunc());
+        public override string ToString() => Match(
+            ex => $"Exception({ex.Message})",
+            t => $"Success({t})");
+    }
+}
---
+++ FunctionalCSharp/src/MarsonShine.Functional/Try.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsonShine.Functional
+{
+    public delegate Exceptional<T> Try<T>();
+
+    public static partial class F
+    {
+        public static Try<T> Try<T>(Func<T> f) => () => f();
+    }
+    public static class TryExt
+    {
+        public static Exceptional<T> Run<T>(this Try<T> @try)
+        {
+            try
+            {
+                return @try();
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+
+        public static Try<R> Map<T, R>(this Try<T> @try, Func<T, R> f) => () => @try.Run()
+        .Match<Exceptional<R>>(
+            ex => ex,
+            t => f(t)
+            );
+
+        // 组合多个Try
+        public static Try<R> Bind<T, R>(this Try<T> @try, Func<T, Try<R>> f) => () => @try.Run().Match(
+            ex => ex,
+            t => f(t).Run());
+    }
+}
