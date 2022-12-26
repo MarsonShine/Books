2b2c2a72c0924b39ef32a6bac4a27eecd75d09be
marsonshine <ms27946@outlook.com>
2022-12-26 22:25:05 +0800

    fix: stream cache i/o implementation

--- APUE/src/5-12/print_buf.c
+++ APUE/src/5-12/print_buf.c
@@ -50,3 +50,62 @@ int is_unbuffered(FILE *fp)
 {
     return (fp->_flags & _IO_UNBUFFERED);
 }
+
+int is_linebuffered(FILE *fp)
+{
+    return (fp->_flags & _IO_LINE_BUF);
+}
+
+int buffer_size(FILE *fp)
+{
+    return (fp->_IO_buf_end - fp->_IO_buf_base);
+}
+#elif defined(__SNBF)
+
+int is_unbuffered(FILE *fp)
+{
+    return (fp->_flags & __SNBF);
+}
+
+int is_linebuffered(FILE *fp)
+{
+    return (fp->_flags & __SLBF);
+}
+
+int buffer_size(FILE *fp)
+{
+    return (fp->_bf._size);
+}
+
+#elif defined(_IONBF)
+
+#ifdef _LP64
+#define _flag __pad[4]
+#define _ptr __pad[1]
+#define _base __pad[2]
+#endif
+
+int is_unbuffered(FILE *fp)
+{
+    return (fp->_flags & _IONBF);
+}
+
+int is_linebuffered(FILE *fp)
+{
+    retunr (fp->_flags & _IOLBF);
+}
+
+int buffer_size(FILE *fp)
+{
+#ifdef _LP64
+    return (fp->_base - fp->_ptr);
+#else
+    return (BUFSIZ);
+#endif
+}
+
+#else
+
+#error unknown stdio implementation!
+
+#endif
