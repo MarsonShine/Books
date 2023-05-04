b4896f7316168d0cc180f3c19f0ebf8086a2073d
marsonshine <ms27946@outlook.com>
2023-05-04 18:47:06 +0800

    feat: metrics

---
+++ CSharpGuide/diagnostics/MetricDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MetricDemo;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static Meter s_meter = new Meter("HatCo.HatStore", "1.0.0");
+    static Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hatsSold");
+    static void Main(string[] args)
+    {
+        #region Demo1
+        //Console.WriteLine("Press any key to exit");
+        //while (!Console.KeyAvailable)
+        //{
+        //    // 模拟销售，每1秒出售4个帽子
+        //    Thread.Sleep(1000);
+        //    s_hatsSold.Add(4);
+        //} 
+        #endregion
+
+        #region Demo2
+        //Program2.Main2(args);
+        #endregion
+        
+        #region Demo3
+        //Program3.Main3(args);
+        #endregion
+
+        #region Demo4
+        Program4.Main4(args);
+        #endregion
+    }
+}
---
+++ CSharpGuide/diagnostics/MetricDemo/Program3.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace MetricDemo
+{
+    class Program3
+    {
+        static Meter s_meter = new Meter("HatCo.HatStore", "1.0.0");
+        static Counter<int> s_hatsSold = s_meter.CreateCounter<int>(name: "hats-sold",
+                                                                    unit: "Hats",
+                                                                    description: "The number of hats sold in our store");
+
+        public static void Main3(string[] args)
+        {
+            Console.WriteLine("Press any key to exit");
+            while (!Console.KeyAvailable)
+            {
+                // Pretend our store has a transaction each 100ms that sells 4 hats
+                Thread.Sleep(100);
+                s_hatsSold.Add(4);
+            }
+        }
+    }
+}
