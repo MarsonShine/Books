1579bbf1718f7b82881055fe5a36ab4faaa1f905
marsonshine <ms27946@outlook.com>
2023-05-09 16:27:19 +0800

    feat: collection metric

---
+++ CSharpGuide/diagnostics/CollectionMetricDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CollectionMetricDemo;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography;
+
+class Program
+{
+    static Meter s_meter = new Meter("HatCo.HatStore", "1.0.0");
+    static Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hats-sold");
+
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Press any key to exit");
+        //while (!Console.KeyAvailable)
+        //{
+        //    // 模拟每秒出售4个帽子
+        //    Thread.Sleep(1000);
+        //    s_hatsSold.Add(RandomNumberGenerator.GetInt32(1,10));
+        //}
+
+        #region Demo2
+        PromethusProgram.Main2(args);
+        #endregion
+    }
+}
