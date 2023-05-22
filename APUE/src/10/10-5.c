2f2af7d82cd8ad208ec80e1da54ca31d16eadcb3
marsonshine <ms27946@outlook.com>
2023-05-22 19:02:22 +0800

    fix: signal,kill,raise

--- APUE/src/10/10-5.c
+++ APUE/src/10/10-5.c
@@ -28,3 +28,4 @@ int main(void)
 // 检查 core 文件，可以从中看出 main 已经调用了 getpwnam 函数，但当 getpwnam 调用 free 时，信号处理程序中断它的运行，并调用 getpwnam 函数，这时 getpwnam 函数又调用了 free。
 // 在信号处理程序调用 free 而主程序也在调用 free 时，malloc 和 free 维护的数据结构就会遭到破坏。
 // 程序运行结果也是随机的：正常运行数秒，然后因为产生 SIGSEGV 信号而终止。在捕捉到信号后，若 main 函数仍在正确运行，其返回值却有时错误，有时正确。
+
