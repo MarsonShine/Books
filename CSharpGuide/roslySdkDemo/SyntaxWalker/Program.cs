2b6350c8b400b537f8ce226d35f7ae0216eaebbb
marsonshine <ms27946@outlook.com>
2023-06-14 14:13:56 +0800

    feat: specific handle syntaxnode

---
+++ CSharpGuide/roslySdkDemo/SyntaxWalker/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SyntaxWalker;
+using static System.Console;
+
+Console.WriteLine("Hello, World!");
+
+const string programText =
+@"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace TopLevel
+{
+    using Microsoft;
+    using System.ComponentModel;
+
+    namespace Child1
+    {
+        using Microsoft.Win32;
+        using System.Runtime.InteropServices;
+
+        class Foo { }
+    }
+
+    namespace Child2
+    {
+        using System.CodeDom;
+        using Microsoft.CSharp;
+
+        class Bar { }
+    }
+}";
+
+SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+var collector = new UsingCollector();
+collector.Visit(root);
+foreach (var directive in collector.Usings)
+{
+    WriteLine(directive.Name);
+}
---
+++ CSharpGuide/roslySdkDemo/SyntaxWalker/UsingCollector.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static System.Console;
+
+namespace SyntaxWalker
+{
+    /// <summary>
+    /// 只关心特定指定操作（如 using 指令）
+    /// </summary>
+    public class UsingCollector : CSharpSyntaxWalker
+    {
+        public ICollection<UsingDirectiveSyntax> Usings { get; } = new List<UsingDirectiveSyntax>();
+
+        public override void VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            WriteLine($"\tVisitUsingDirective called with {node.Name}.");
+            if (node?.Name?.ToString() != "System" && !node!.Name!.ToString().StartsWith("System."))
+            {
+                WriteLine($"\t\tSuccess. Adding {node.Name}.");
+                Usings.Add(node);
+            }
+        }
+    }
+}
