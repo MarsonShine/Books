e73b643e651809f6fcf0504d7b59803c9de5a36f
marsonshine <ms27946@outlook.com>
2023-07-05 11:44:39 +0800

    fix: vectorization guidelines

--- CSharpGuide/simd-vector-demo/Program.cs
+++ CSharpGuide/simd-vector-demo/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using simd_vector_demo;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 Console.WriteLine("Hello, World!");
 
@@ -23,3 +25,37 @@ Vector128<float> r = Vector128.GreaterThan(left2,right2);
 Vector128<float> result = Vector128.ConditionalSelect(r,left2,right2);
 
 Console.WriteLine(Vector128.Create(4.0f,3,3,4) == result);
+
+// 加宽和缩小
+byte[] byteBuffer = Enumerable.Range('A', 128 / 8).Select(i => (byte)i).ToArray();
+Vector128<byte> byteVector = Vector128.Create(byteBuffer);
+Console.WriteLine(byteVector);
+(Vector128<ushort> Lower, Vector128<ushort> Upper) = Vector128.Widen(byteVector);
+Console.Write(Lower.AsByte());
+Console.WriteLine(Upper.AsByte());
+
+Vector256<ushort> ushortVector = Vector256.Create(Lower, Upper);
+Console.WriteLine(ushortVector);
+Span<ushort> ushortBuffer = stackalloc ushort[256 / 16];
+ushortVector.CopyTo(ushortBuffer);
+Span<char> charBuffer = MemoryMarshal.Cast<ushort, char>(ushortBuffer);
+Console.WriteLine(new string(charBuffer));
+
+Vector256<ushort> ushortVector2 = Vector256.Create((ushort)300);
+Console.WriteLine(ushortVector2);
+unchecked { Console.WriteLine((byte)300); } // 255超出了byte的范围，所以会溢出；溢出的部分从位置0开始计算，所以结果是44
+Console.WriteLine(300 & byte.MaxValue);
+Console.WriteLine(ushortVector2.GetLower());
+Console.WriteLine(ushortVector2.GetUpper());
+Console.WriteLine(VectorOperator.Narrow(ushortVector2.GetLower(), ushortVector2.GetUpper()));
+
+if (Sse2.IsSupported)
+{
+    Console.WriteLine(Sse2.PackUnsignedSaturate(ushortVector2.GetLower().AsInt16(), ushortVector2.GetUpper().AsInt16()));
+}
+
+// 洗牌
+Vector128<int> intVector = Vector128.Create(100, 200, 300, 400);
+Console.WriteLine(intVector);
+// 通过 Shuffle 方法，我们可以将向量中的元素重新排列，下面实现了一个逆序的洗牌
+Console.WriteLine(Vector128.Shuffle(intVector, Vector128.Create(3, 2, 1, 0)));
--- CSharpGuide/simd-vector-demo/VectorOperator.cs
+++ CSharpGuide/simd-vector-demo/VectorOperator.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
 
@@ -17,6 +18,39 @@ namespace simd_vector_demo
             return (vector & Vector128.Create((byte)0b_1000_0000)) == Vector128<byte>.Zero;
             // 也可以使用 ExtractMostSignificantBits 方法
             return vector.ExtractMostSignificantBits() == 0;
+        }
+
+        public static unsafe Vector128<byte> Narrow(Vector128<ushort> lower, Vector128<ushort> upper)
+        {
+            Unsafe.SkipInit(out Vector128<byte> result);
+
+            for (int i = 0; i < Vector128<ushort>.Count; i++)
+            {
+                byte value = (byte)GetElementUnsafe(lower, i);
+                SetElementUnsafe(result, i, value);
+            }
+
+            for (int i = Vector128<ushort>.Count; i < Vector128<byte>.Count; i++)
+            {
+                byte value = (byte)GetElementUnsafe(upper, i - Vector128<ushort>.Count);
+                SetElementUnsafe(result, i, value);
+            }
+
+            return result;
+        }
+
+        static T GetElementUnsafe<T>(in Vector128<T> vector, int index)
+            where T : struct
+        {
+            Debug.Assert((index >= 0) && (index < Vector128<T>.Count));
+            return Unsafe.Add(ref Unsafe.As<Vector128<T>, T>(ref Unsafe.AsRef(in vector)), index);
+        }
+
+        static void SetElementUnsafe<T>(in Vector128<T> vector, int index, T value)
+            where T : struct
+        {
+            Debug.Assert((index >= 0) && (index < Vector128<T>.Count));
+            Unsafe.Add(ref Unsafe.As<Vector128<T>, T>(ref Unsafe.AsRef(in vector)), index) = value;
         }
     }
 }
