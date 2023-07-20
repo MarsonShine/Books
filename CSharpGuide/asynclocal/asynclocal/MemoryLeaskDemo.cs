d292e001ba73e3ef947a35e3823c678bab8d7d96
marsonshine <ms27946@outlook.com>
2023-07-20 18:23:58 +0800

    feat: AsyncLocal<T> best practice

---
+++ CSharpGuide/asynclocal/asynclocal/MemoryLeaskDemo.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 本有一个单例的 NumberCache，用于存储一小时内的数字。我们有一个 ChunkyObject，它在一个字段中存储了 32K 字符串，并且有一个异步本地，因此任何运行中的代码都可以访问当前的 ChunkyObject。该对象本应在 GC 运行时被收集，但我们却通过 CancellationToken.Register 隐式地在 NumberCache 中捕获了 ChunkyObject。
+
+与其说我们只是在缓存数字和 CancellationTokenSource，不如说我们是在隐式捕获并存储所有连接到当前执行上下文的异步本地对象一个小时！
+ */
+namespace asynclocal
+{
+    internal class MemoryLeaskDemo
+    {
+        public static void Start()
+        {
+            var cache = new NumberCache(TimeSpan.FromHours(1));
+
+            ExecutionContext executionContext = ExecutionContext.Capture()!;
+            // 模拟 10000 次并发请求
+            Parallel.For(0, 10000, i =>
+            {
+                // 根据请求恢复初始执行的 ExecutionContext
+                ExecutionContext.Restore(executionContext);
+
+                //ChunkyObject.Current = new ChunkyObject();
+
+                //cache.Add(i);
+
+                SafeChunkyObject.Current = new SafeChunkyObject();
+                cache.Add(i);
+                // 清空大块对象，以便 GC 释放内存
+                SafeChunkyObject.Current = null;
+            });
+
+            Console.WriteLine("Before GC: " + BytesAsString(GC.GetGCMemoryInfo().HeapSizeBytes));
+            Console.ReadLine();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("After GC: " + BytesAsString(GC.GetGCMemoryInfo().HeapSizeBytes));
+            // 实际上并没有被 GC
+            Console.ReadLine();
+
+            static string BytesAsString(long bytes)
+            {
+                string[] suffix = { "B", "KB", "MB", "GB", "TB" };
+                int i;
+                double doubleBytes = 0;
+
+                for (i = 0; bytes / 1024 > 0; i++, bytes /= 1024)
+                {
+                    doubleBytes = bytes / 1024.0;
+                }
+
+                return string.Format("{0:0.00} {1}", doubleBytes, suffix[i]);
+            }
+        }
+    }
+
+    public class NumberCache
+    {
+        private readonly ConcurrentDictionary<int, CancellationTokenSource> _cache = new();
+        private readonly TimeSpan _timeSpan;
+
+        public NumberCache(TimeSpan timeSpan)
+        {
+            _timeSpan = timeSpan;
+        }
+
+        public void Add(int key)
+        {
+            var cts = _cache.GetOrAdd(key, _ => new CancellationTokenSource());
+            // Delete entry on expiration
+            cts.Token.Register((_, _) => _cache.TryRemove(key, out _), null);
+            //cts.Token.UnsafeRegister((_, _) => _cache.TryRemove(key, out _), null); // 这样就不会捕获 ChunkyObject 了。或者是用 cts.Token.Register，在模拟请求代码段的最后，设置 ChunkyObject.Current = null
+
+            // Start count down
+            cts.CancelAfter(_timeSpan);
+        }
+    }
+    class ChunkyObject
+    {
+        private static readonly AsyncLocal<ChunkyObject?> _current = new();
+
+        // Stores lots of data (but it should be gen0)
+        private readonly string _data = new('A', 1024 * 32);
+
+        // 存在内存泄漏问题
+        public static ChunkyObject? Current
+        {
+            get => _current.Value;
+            set => _current.Value = value;
+        }
+
+        public string Data => _data;
+    }
+
+    class SafeChunkyObject
+    {
+        private static readonly AsyncLocal<StrongBox<SafeChunkyObject?>?> _current = new();
+
+        // Stores lots of data (but it should be gen0)
+        private readonly string _data = new('A', 1024 * 32);
+
+        public static SafeChunkyObject? Current
+        {
+            get => _current.Value?.Value;
+            set
+            {
+                var box = _current.Value;
+                if (box is not null)
+                {
+                    // 更改任何被复制的执行上下文中的值
+                    box.Value = null;
+                }
+                if (value is not null)
+                {
+                    _current.Value = new StrongBox<SafeChunkyObject?>(value);
+                }
+            }
+        }
+
+        public string Data => _data;
+    }
+
+}
---
+++ CSharpGuide/asynclocal/asynclocal/Program.cs
@@ -0,0 +1,134 @@
+﻿// refference: https://github.com/davidfowl/AspNetCoreDiagnosticScenarios/blob/master/AsyncGuidance.md#asynchronous-programming
+// See https://aka.ms/new-console-template for more information
+using asynclocal;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+
+//using (var thing = new DisposableThing())
+//{
+//    DisposableThing.Current = thing;
+//    Dispatch();
+//    DisposableThing.Current = null;
+//}
+
+// 上述示例将始终导致抛出 ObjectDisposedException。尽管 Log 方法在记录值之前会防御性地检查是否为空，但它仍持有已释放的 DisposableThing 的引用。将 AsyncLocal<DisposableThing> 设置为 null 不会影响 Log 内部的代码，这是因为在写入时执行上下文是写时复制（COW）的。这意味着今后读取的所有 DisposableThing.Current 都将为空，但不会影响之前的任何读取
+
+// 每次给 AsyncLocal 设置新值时，执行上下文的哈希代码都是不同的。
+DisposableThing.Current = new DisposableThing();
+Console.WriteLine("After setting thing " + ExecutionContext.Capture()?.GetHashCode());
+// 当我们设置 DisposableThing.Current = null 时，我们是在创建一个新的执行上下文，而不是更改由 Task.Run 捕捉到的执行上下文
+DisposableThing.Current = null;
+Console.WriteLine("After setting Current to null " + ExecutionContext.Capture()?.GetHashCode());
+Console.ReadLine();
+
+// 上面都是 AsyncLocal 持有的对象不可变的情况，如果想让 AsyncLocal 持有的对象可变
+// 可让 AsyncLocal 持有一个引用对象，这样虽然每次更改值时，会创建的新的 ExecuteContext，但内部持有的对象是同一个引用
+DisposableThing2.Current = new DisposableThing2();
+
+Console.WriteLine("After setting thing " + ExecutionContext.Capture()?.GetHashCode());
+
+DisposableThing2.Current = null;
+
+Console.WriteLine("After setting Current to null " + ExecutionContext.Capture()?.GetHashCode());
+// 要注意的是，当我们持有引用对象时，在处理异步、并发的情况下，要注意线程安全的问题
+
+MemoryLeaskDemo.Start();
+
+void Dispatch()
+{
+    // Task.Run will capture the current execution context (which means async locals are captured in the callback)
+    _ = Task.Run(async () =>
+    {
+        // Delay for a second then log
+        await Task.Delay(1000);
+
+        Log();
+    });
+}
+
+void Log()
+{
+    try
+    {
+        // Get the current value and make sure it's not null before reading the value
+        var thing = DisposableThing.Current;
+        if (thing is not null)
+        {
+            // 这里尽管校验是否为null，但是还是会报错，因为在这个异步方法中
+            // 当前的AsyncLocal持有的对象已经被释放了
+            Console.WriteLine($"Logging ambient value {thing.Value}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+internal class DisposableThing : IDisposable
+{
+    private static readonly AsyncLocal<DisposableThing?> _current = new();
+
+    private bool _disposed;
+    public static DisposableThing? Current
+    {
+        get => _current.Value;
+        set
+        {
+            _current.Value = value;
+        }
+    }
+
+    public int Value
+    {
+        get
+        {
+            if (_disposed) throw new ObjectDisposedException(GetType().FullName);
+            return 1;
+        }
+    }
+
+    public void Dispose()
+    {
+        _disposed = true;
+    }
+}
+
+internal class DisposableThing2 : IDisposable
+{
+    private static readonly AsyncLocal<StrongBox<DisposableThing2?>?> _current = new();
+
+    private bool _disposed;
+    public static DisposableThing2? Current
+    {
+        get => _current.Value?.Value;
+        set
+        {
+            var box = _current.Value;
+            if (box is not null)
+            {
+                // 更改任何被复制的执行上下文中的值
+                box.Value = null;
+            }
+            if (value is not null)
+            {
+                _current.Value = new StrongBox<DisposableThing2?>(value);
+            }
+        }
+    }
+
+    public int Value
+    {
+        get
+        {
+            if (_disposed) throw new ObjectDisposedException(GetType().FullName);
+            return 1;
+        }
+    }
+
+    public void Dispose()
+    {
+        _disposed = true;
+    }
+}
