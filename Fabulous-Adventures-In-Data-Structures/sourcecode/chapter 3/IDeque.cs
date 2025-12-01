07b00fe5946458e5ab3b5c9fdceb5130dfd9526a
marsonshine <ms27946@outlook.com>
2025-12-01 14:22:19 +0800

    Add chapter 3 deque sample and update finger tree text

---
+++ Fabulous-Adventures-In-Data-Structures/sourcecode/chapter 3/IDeque.cs
@@ -0,0 +1,14 @@
+﻿namespace chapter_3
+{
+    public interface IDeque<T> : IEnumerable<T>
+    {
+        IDeque<T> PushLeft(T item);
+        IDeque<T> PushRight(T item);
+        IDeque<T> PopLeft();
+        IDeque<T> PopRight();
+        T Left();
+        T Right();
+        bool IsEmpty();
+        IDeque<T> Concatenate(IDeque<T> items);
+    }
+}
